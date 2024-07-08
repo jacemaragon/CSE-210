8b2b7e996e641d8885e25120f2d867dca9bfe3d0
jacemaragon <jacemaragon@gmail.com>
2024-07-08 13:13:14 -0700

    change

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,7 +7,7 @@ class Program
         Fraction f1 = new Fraction();
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
-
+//code
         Fraction f2 = new Fraction(5);
         Console.WriteLine(f2.GetFractionString());
         Console.WriteLine(f2.GetDecimalValue());
